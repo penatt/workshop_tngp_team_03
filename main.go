e13e1efea65709f35ca00778fd3adc382cb88e25
Phukhao_Napat <napat.ji@tnds.com>
2024-08-16 17:40:12 +0700

    fix:division by zero

--- main.go
+++ main.go
@@ -8,18 +8,21 @@ func minus(a int, b int) int {
 	return a - b
 }
 
-func Divide(a, b int) int {
-	var result int
-	result = a / b
-	return result
+func Divide(a, b int) (int, string) {
+	if b != 0 {
+		var result int
+		result = a / b
+		return result, "ok"
+	}
+	return 0, "Error: Division by zero"
 }
 
 func multiply(a, b int) int {
 	return a * b
 }
 
-func sum(a int,b int)int{
-	return a+b
+func sum(a int, b int) int {
+	return a + b
 }
 
 func mod(a, b int) int {
@@ -31,15 +34,19 @@ func main() {
 
 	a := 40
 	b := 50
-	
+
 	result := minus(a, b)
-	result_divide := Divide(a, b)
+	result_divide, err := Divide(a, b)
+	if err == "ok" {
+		fmt.Printf("%d / %d = %d\n", a, b, result_divide)
+	} else {
+		fmt.Println(err)
+	}
 	result_multiply := multiply(a, b)
 	fmt.Printf("Output:")
 	fmt.Printf("%d - %d = %d\n", a, b, result)
-	fmt.Printf("%d / %d = %d\n", a, b, result_divide)
-	fmt.Printf("a * b = %v * %v = %v\n", a, b,result_multiply)
-	fmt.Printf("a + b = %v + %v = %v\n", a,b, sum(a,b))
-	fmt.Printf("%v '%%' %v = %v\n", a, b, mod(a,b))
-	fmt.Printf("%v '%%' %v = %v\n", a, b, mod(a,b))
+	fmt.Printf("a * b = %v * %v = %v\n", a, b, result_multiply)
+	fmt.Printf("a + b = %v + %v = %v\n", a, b, sum(a, b))
+	fmt.Printf("%v '%%' %v = %v\n", a, b, mod(a, b))
+	fmt.Printf("%v '%%' %v = %v\n", a, b, mod(a, b))
 }
